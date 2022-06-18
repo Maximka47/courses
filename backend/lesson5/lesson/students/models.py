f14e1be17ee5a39cd48d379be55c0b6ef8051224
Maximka47 <user2017max@gmail.com>
2022-06-19 00:41:09 +0300

    lesson5
    Django start

---
+++ backend/lesson5/lesson/students/models.py
@@ -0,0 +1,55 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+class StudentGroup(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    students_number = models.DecimalField(max_digits=4, decimal_places=0,
+                                                        verbose_name="Number of students",
+                                                        blank=True,
+                                                        null=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Group of students"
+        verbose_name_plural="Groups of students"
+
+class Student(models.Model):
+
+    SURNAME_CHOICES = (
+        ("Petrenko","Петренко"),
+        ("Shevchenko","Шевченко"),
+    )
+
+    name = models.CharField(verbose_name="Name",
+                            max_length=100,
+                            blank=True,
+                            null=True)
+
+    surname = models.CharField(verbose_name="Surname",
+                            max_length=100,
+                            blank=True,
+                            null=True,
+                            choices=SURNAME_CHOICES)
+
+    birthday = models.DateField(verbose_name="Birthday",
+                                blank=True,
+                                null=True)
+
+    group = models.ForeignKey(StudentGroup, verbose_name="Group",   #OneToOneField(one student in one group) ManyToManyField(many students and each of them can be in many groups)
+                                            on_delete=models.SET_NULL,
+                                            null=True,
+                                            blank=True)
+    # group_id = models.IntegerField()
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.name += '111'
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name="Student"
+        verbose_name_plural="Students"
